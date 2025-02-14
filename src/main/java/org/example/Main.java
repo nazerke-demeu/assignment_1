469aee0904978590c7a5341301bec4c953bc3fea
nazerke <demeuova.2005@gmail.com>
2025-02-15 02:06:43 +0500

    Final Result

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Team team = context.getBean(Team.class);
+
+        System.out.println("Team: " + team.getTeamName());
+        System.out.println("Player: " + team.getPlayer().getName() +
+                " (№" + team.getPlayer().getNumber() + ", " + team.getPlayer().getPosition() + ")");
     }
 }
